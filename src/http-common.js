a83161135dd4f9f7e09ba16050d8e432c63af4dc
alejandraquimi <alevquim@espol.edu.ec>
2022-01-07 19:59:22 -0500

    mm

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://mysqldesployheroku.herokuapp.com/api",
+  baseURL: "https://mysqldesployheroku.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
